400ae7033ede5b11ef82dfe3287a90d09172bc54
chetan-1487 <chetan@thinksys.com>
2025-02-18 13:00:42 +0530

    18-Feb-2025

--- OOPS/Object.py
+++ OOPS/Object.py
@@ -39,4 +39,122 @@ shapes=[rectangle(2,3),square(2)]
 for shape in shapes:
     print(shape.area())
 
+
+# Overriding polymorphism
+class Animal:
+    def sound(self):
+        return "Some generic animal sound"
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
+
+Dog=Dog()
+print(Dog.sound())
+
+#Using function
+
+def hello(animal):
+    return animal.sound()
+class Dog:
+    def sound(self):
+        return "Bark"
+class Cat:
+    def sound(self):
+        return "Meow"
+animals=[Dog(),Cat()]
+for animal in animals:
+    print(hello(animal))
+
 #Inheritence
+
+#Single Inheritance
+
+class hello:
+    def kam(self):
+        return "Hello world"
+class world(hello):
+    def kam(self):
+        return "Hello"
+a=world()
+print(a.kam())
+
+#Multiple Inheritance
+
+class animal:
+    def sound(self):
+        return "Hello"
+class Mammals:
+    def behave(self):
+        return "World"
+class Dog(animal,Mammals):
+    def manage(self):
+        return "abc"
+dog=Dog()
+print(dog.manage())
+print(dog.behave())
+print(dog.sound())
+
+# MultiLevel Inheritance
+
+class Animal:
+    def speak(self):
+        print("Animal speaks")
+
+class Mammal(Animal):
+    def has_fur(self):
+        print("Mammal has fur")
+
+class Dog(Mammal):
+    def bark(self):
+        print("Dog barks")
+
+dog = Dog()
+dog.speak()
+dog.has_fur()
+dog.bark()
+
+#Hierrchical Inheritance
+
+class Animal:
+    def sound(self):
+        return "Some generic animal sound"
+
+class Dog(Animal):
+    def sound(self):
+        return "Bark"
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
+
+Dog=Dog()
+print(Dog.sound())
+
+#Hybrid Inheritance
+
+class Animal:
+    def world(self):
+        return "Some generic animal sound"
+
+class Dog(Animal):
+    def hhello(self):
+        return "Bark"
+
+class Cat(Animal):
+    def sound(self):
+        return "Meow"
+class Abc:
+    def ding(self):
+        return "Maintain"
+class Cow(Animal,Abc):
+    def Manage(self):
+        return "Buddy"
+
+Dog=Cat()
+print(Dog.sound())
+print(Dog.world())
